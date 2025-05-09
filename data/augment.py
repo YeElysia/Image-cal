a064bc736397386af1c632a3e017ae3ff076b01f
yee <3330637731@qq.com>
2025-05-09 22:43:35 +0800

    数据增强修改

--- data/augment.py
+++ data/augment.py
@@ -4,12 +4,7 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 
-
-
-def get_transforms(augment: bool = True) -> tuple[A.Compose, A.Compose]:
-    # 定义训练和验证的转换
-    if augment:
-        train_transform = A.Compose([
+classify_augmentations = A.Compose([
             A.HorizontalFlip(p=0.5), # 随机水平翻转
             A.Resize(height=224, width=224),
             A.RandomCrop(height=224, width=224, p=0.5), # 随机裁剪
@@ -20,16 +15,8 @@ def get_transforms(augment: bool = True) -> tuple[A.Compose, A.Compose]:
             ToTensorV2(),
         ])
 
-        val_transform = A.Compose([
-            A.Resize(height=224, width=224),
-            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-            ToTensorV2(),
-        ])
-        return train_transform, val_transform
-    else:
-        transform = A.Compose([
-            A.Resize(height=224, width=224),
-            A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-            ToTensorV2(),
-        ])
-        return transform, transform
+classify_transforms = A.Compose([
+    A.Resize(height=224, width=224),
+    A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    ToTensorV2(),
+])
